9bfb54e083b6177c53ca08443874cf44259e6ab4
Osmand Das <osmandpdas@gmail.com>
2018-12-01 23:54:13 -0500

    updated message

--- src/main/java/org/osmand/didemo/services/ConstructorGreetingService.java
+++ src/main/java/org/osmand/didemo/services/ConstructorGreetingService.java
@@ -4,8 +4,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ConstructorGreetingService implements GreetingService {
-
+	
+	public static final String GREETINGS = "Hello, Good evening";
     public String sayGreeting(){
-        return "Hello, this is Constructor Greeting Service.";
+        return GREETINGS;
     }
 }
